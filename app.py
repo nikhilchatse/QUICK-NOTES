36e44bad175eb9b212b7330b59977f6c92608e0d
Nikhil-Chatse <nikhilchatse143@gmail.com>
2025-12-28 15:15:55 +0530

    flask app for render deployement

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import *
 import sqlite3
+import os
 
 app=Flask(__name__)
 app.secret_key="secret123"
@@ -164,4 +165,5 @@ def updatesave():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
